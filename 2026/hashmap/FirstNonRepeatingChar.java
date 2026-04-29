bfd2a95ae99bc75110f7da074851ae2a9c0722a5
sandeepnegi1996 <9041128179sandy@gmail.com>
2026-04-29 18:16:01 +0530

    added two hashmap programs

---
+++ 2026/hashmap/FirstNonRepeatingChar.java
@@ -0,0 +1,49 @@
+// ** Find the first non repeating character 
+// ** example in the below mentioned string there are multiple characters that are repeated but i want the first character that is repeated 
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class FirstNonRepeatingChar {
+    public static void main(String[] args) {
+        
+        String name= "ssaannddep";
+
+        // with hashmap we can find the frequency but we will lost the order with linkedHashMap the order will be maintained.
+
+     char []arr = name.toCharArray();
+
+     LinkedHashMap<Character,Integer> map = new LinkedHashMap<>();
+     for (int i = 0; i < arr.length; i++) {
+
+
+        if (map.containsKey(arr[i])) {
+
+            map.put(arr[i], map.get(arr[i])+1);
+            
+        }else {
+            map.put(arr[i],1);
+        }
+        
+
+     }
+
+     map.forEach((k,v)->{
+
+        System.out.println(k+"  :   "+v);
+     });
+
+
+     //       Set<Map.Entry<Integer,Integer>> entries=  map.entrySet();
+
+Optional<Character> optionalIsFirstNonRepeating = map.entrySet().stream()
+        .filter(entry -> entry.getValue().toString().equals("1") )
+        .map(Map.Entry::getKey)
+        .findFirst();
+
+    
+
+    System.out.println(optionalIsFirstNonRepeating);
+    }
+}
--- 2026/hashmap/FrequencyCounter.java
+++ 2026/hashmap/FrequencyCounter.java
@@ -1,13 +1,17 @@
 //problem statement 
 // 1. find the frequency of each element in an array 
+// 2. find the element which is most frequent 
 
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 public class FrequencyCounter {
 
      public static void main(String[] args) {
         
-        int []arr ={1,2,3,4,5,1,2,3};
+        int []arr ={1,2,3,4,5,1,2,3,1,1,1};
 
         HashMap<Integer,Integer> map =  new HashMap<>();
 
@@ -28,6 +32,15 @@ public class FrequencyCounter {
          System.out.println("key is :  "+k +" value is :  "+v);
         });
 
+
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()) {
+         System.out.println(entry.getKey()+"   :  "+entry.getValue());
+        }
+
+
+
+
+       
 
      }
 }
